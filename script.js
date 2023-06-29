a9d8d2a1dfb54d08d0d45876837d89bc27fb5096
voltage <128322069+voltage-developer@users.noreply.github.com>
2023-06-29 18:03:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("woking")
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+  const navLinks = document.querySelectorAll("nav a");
+
+  for (const link of navLinks) {
+    link.addEventListener("click", smoothScroll);
+  }
+
+  function smoothScroll(e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    window.scrollTo({
+      top: targetElement.offsetTop,
+      behavior: "smooth"
+    });
+  }
+});
